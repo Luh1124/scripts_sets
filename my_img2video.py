3b3f6dfe29e3b61450fdc67594d939c58d0ae333
li.hong_203673 <li.hong_203673@hellogroup.com>
2022-07-29 21:40:35 +0800

    bug

--- my_img2video.py
+++ my_img2video.py
@@ -12,17 +12,26 @@ from itertools import cycle
 from torch.multiprocessing import Pool, Process
 
 def run(video_info):
+
     video_name, video_id, opt = video_info
     image_names = list()
-    for ext in extensions:
+    for ext in opt.extensions:
         image_names += glob.glob(f'{opt.input_dir}/{video_name}/*.{ext}', recursive=True)
     image_names = sorted(image_names)
+    if len(image_names)==0:
+        print("video_name is empty")
+        return
     image_list = list()
     for img_id, image_name in tqdm(enumerate(image_names),total=len(image_names),desc=f"{video_id}/{opt.video_length}"):
-        image = Image.open(image_name).convert("RGB")
+        image = Image.open(image_name).convert("RGB").resize((opt.size,opt.size))
         image_list.append(image)
-    
-    imageio.mimsave(f'{opt.output_dir}/{video_name}', image_list, opt.fps)  
+   
+    video_name=video_name if video_name[:-4]==".mp4" else video_name + '.mp4' 
+    # print(image_list, video_name)
+    # print(f'{opt.output_dir}/{video_name}')
+    imageio.mimsave(os.path.join(opt.output_dir,video_name), image_list, fps=opt.fps)  
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -33,7 +42,9 @@ if __name__ == '__main__':
                         default="path_to_videos",
                         help='the folder of the output files')
     parser.add_argument('--fps', type=int, default=25)
-    parser.add_argument('--workers', type=int, default=4)
+    parser.add_argument('--size', type=int, default=256)
+    
+    parser.add_argument('--workers', type=int, default=1)
 
     opt = parser.parse_args()
     os.makedirs(opt.output_dir, exist_ok=True)
@@ -41,13 +52,15 @@ if __name__ == '__main__':
     IMAGE_EXTENSIONS_LOWERCASE = {'jpg', 'png', 'jpeg', 'webp'}
     IMAGE_EXTENSIONS = IMAGE_EXTENSIONS_LOWERCASE.union({f.upper() for f in IMAGE_EXTENSIONS_LOWERCASE})
     extensions = IMAGE_EXTENSIONS
-
+    opt.extensions=extensions
+    
     pool = Pool(opt.workers)
     args_list = cycle([opt])
     video_list=sorted(os.listdir(opt.input_dir))
     opt.video_length = len(video_list)
+    # import pdb;pdb.set_trace()
     video_ids=range(opt.video_length)
-    print('Total number of videos:', len(opt.video_length))
+    print('Total number of videos:', opt.video_length)
     for data in pool.imap_unordered(run, zip(video_list, video_ids, args_list)):
         None
     
